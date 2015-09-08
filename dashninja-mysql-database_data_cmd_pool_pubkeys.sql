1ae12140566f34cc06b08ee9c1f796feb9c51019
Alexandre Devilliers <elbereth@dashninja.pl>
2015-09-08 03:48:56 +0200

    Added data dump for cmd_pools_pubkey

---
+++ dashninja-mysql-database_data_cmd_pool_pubkeys.sql
@@ -0,0 +1,38 @@
+--
+-- This file is part of Dash Ninja.
+-- https://github.com/elbereth/dashninja-db
+--
+-- Dash Ninja is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dash Ninja is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+--
+
+--
+-- Dumping data for table `cmd_pools_pubkey`
+--
+
+LOCK TABLES `cmd_pools_pubkey` WRITE;
+/*!40000 ALTER TABLE `cmd_pools_pubkey` DISABLE KEYS */;
+INSERT INTO `cmd_pools_pubkey` VALUES (0,'P2POOL','p2pool'),(1,'P2POOL','p2pool'),(0,'XajWfJGecki3UDxBUHcwzSCrcU3KsRzQvs ','dashminer.com'),(0,'XamoYgT5pqs2q1Q3mebWsMWZaAn12S5nRj','dark.coinobox.ru'),(0,'XbodwXnt6DrZ3Ncss1QGzqtFZiMKsVQjp9','switchercoincom'),(0,'XbsWaCgT1sBdPjvwYT6q2qnkgme2aVvBXT','dashminer.com'),(0,'Xby219dS5AtrDi943TCPrFCHgwPrWFJYnL','dark.suprnova.cc'),(0,'Xc5bNrUbU3Mq5kvd2XLktzDH5a34mL2aKr','x11ltc1btccom'),(0,'XcCYqNP5BPxFpNxSXMgyL5TQWr82eVgNtH','cpupool.net'),(0,'XcZjBCbAKUpFNf2mrnyNajRg8BQUgyRNhK','dash.poolto.be'),(0,'Xemt2zeYELCkddJHvsW4x9ytqTEnkiviaH','dash.miningpoolhub.com'),(0,'XeqPtR8wodzmgmrxPpoT5HKcfBuRuofF6i','simplemulti'),(0,'XfQ9FPyCRgXvhqkEBhhWtoup53XJAyLXvX','dashminer.com'),(0,'XfTo4DEGRhHD2P6MknkkN1UFVN26HpyAcf','dash.coinobox.com'),(0,'XfgNCeTJxBVHb9CCpn52QyfjfpBmPQUYdA ','suchpool.pw'),(0,'XgAce4WzRVXhrkcXSgZnZ6mNqXtEMsGBWY','drk.cryptoadhd.com'),(0,'XgzghDKKNAj8fWPBFsqkD7jqUBqp9Q1sD1','x11ltc1btccom'),(0,'Xh42G4vhYp5q4Yq6tRJwUuH2JaGthTGL2P','yaamp'),(0,'XhBLSR1HhoaWrdGgrjCHxJkCWWiFFhveHs','nicehash'),(0,'XhPGDSwtQ8PzW2wKd1HKk5KzuwNJP1ox4F','ltcrabbit.com'),(0,'XiCCUohMVDYmq2TTuSJrcij66RrraKFC7G','minepit'),(0,'XiErkr4nsLdCMYBDRCKzWTRfmrjVyXvZtE','ltcrabbit.com'),(0,'XiFrhXFmYjEHPYR92SwnYDHkpgTiKadANr','multipool.us'),(0,'XjB8jaQMHMm25xpP88KnPDdEvqsLgkQCjn','coinotron.com'),(0,'XjE3HCuw27tgxYrjytXn3BZEqVdoxGMV4r','nicehash'),(0,'XjqzWnjnEf8YVeoNcC7m1FwCuQbHa3rJMW','simpledrkcom'),(0,'XkETHbymbZgkpjHG3f4Ad8osWg3XkPta5L','drk.hashlink.eu'),(0,'XkGyZzyzYcLgx9F7RAqth2XydvreNvRXHB','nicehash'),(0,'XkqucPckkuHhpARajBRpwbJKaD8TupLXx6','hashpower.co'),(0,'Xm13g8y85Vm9687EYb2ZsKqgjL161gu4Vm','x11ltc1btccom'),(0,'Xm698KgR3755KfWXTEycZGX5GB36m7tGqt','ipominer.com'),(0,'XmC6ndZ8BSArfuB4Y5eNTLb4MUHo3umG2A','drkcif8com'),(0,'XmSvKaXV1nfxYSytphtMMkeLc6i44i2taT','hashrateorg'),(0,'XmxDXt6VDRRHobBKRQkhGUVzXgDC9STJmA','mining.darkcointalk.org'),(0,'XmzgZvVXWihE8FXbyLUj5YzAvVaHpRd1Gu','dedicatedpoolcom'),(0,'XnNa7Wtru33D5U3T2cdL7ByMsSXALXFzaE','dashminer.com'),(0,'XnTeHzwPcau1gvJVrq4ccYDxfKYFLDJBS8','x11ltc1btccom'),(0,'XpGaGWQMWeRgLe8Fri7b28pTtspBwyvhge','dark.coinobox.ru'),(0,'XqdPRhsJiovaNgenMVNbE3pPEbgewUhnze','dark.coinobox.ru'),(0,'Xqi4JGoYQsKy2uyxyA3nuR1vrJi474jnJj','ghash.io'),(0,'Xs1DJ8dTNDwLFQTSFyzhhk64bQfNV7cBzE','ltcrabbit.com'),(0,'XsF8k8s5CoS3XATqW2FkuTsznbJJzFAC2U','wafflepool'),(0,'XsQeoUUhrfGXWDgiosg1UneCbd3GCaPqa4','xpoolca'),(0,'XsjvcNAJdEptrq8fBPy6yAgnpHgH9Zpb3T','coinotron.com'),(0,'XtF2qEvjrCpKtujA8haSgBjZmVopDWn2ER','x11ltc1btccom'),(0,'XtTwRjpviDMp729eNWLnZMpSDqX8WpbD45','multipool.us'),(0,'XtnDTyEAHanRYqjMbEX7EwM8AeGr4nnbm2','x11ltc1btccom'),(0,'Xto1vYMRbP4uDz31VMBgfkPQwynotrikq6','nicehash'),(0,'Xtu2bVjA6MZ2oVYUsmaVyDjhzt26j6VCUA','x11ltc1btccom'),(0,'Xu7Z65DQjHpAbHD4KPDwVM181gaBf66uru','nicehash'),(0,'XuHwj7zPccqzyNnhMeEbv5zpB67nJ9xB7D','coinotron.com'),(0,'Xv5SgdXEpp1ZncT6RKhNm1FvywC3jhdpwM','coinmine.pw'),(0,'XvDRVCT4FJfZzbHsUxtww2b1Ug8pfpBnym','coinking.io'),(0,'Xw3QYBeN8FwguMJX5qHLJFRvy6AqcVtuRh','coinmine.pw'),(0,'Xw43GxB6uYmGZYwV61vhf46DsPvHY4popZ','suchpool'),(0,'XwE1PA7dJHzEQNuasNeg7K8koLPhXLDxY5','x11.greenpool.tk'),(0,'XwfiFDTgpVdMBiHKtLZ5Q8KzghZ8qgCfxb','coinotron.com'),(0,'Xwsq2T8WLngm3M6usbqCTvGYENXQ9H9yHE','darkcoincryptocurrencytw'),(0,'XwtY5Bcf7KbgvxEuFZK9owBy2YYKpDZST3','xhash.net'),(0,'XxE6xtBFqMWjLBtgcPy5vDxU1ncfZuFEyv','ltcrabbit.com'),(0,'XxN2gt5cWqM6n1Zu2bmYrvaRmcDV1a6XKJ','coinmine.pl'),(0,'XxW3kE1HcTbNNhBEJdR6Lnn3KazG7GeZnk','dashminer.com'),(0,'XxtLGtKEiNZHJTsze5NoEgfhTZLMpNDWpx','yaamp.com'),(0,'XyLz1jG5mUy93hK1QDcPZ6DqDqLvWdEF4A ','xpool.ca'),(0,'XyMGfSC5STveC1rqCBiyH5Jb9GiJVA2gXA','trademybitcom'),(0,'XyWLKQqFqs86e5jQkSmQ2DcZVySAYqVNYS','dashminer.com'),(1,'myo8pCxQKTncVU5bC7VMqnUZqXPecS6t89','chaeplin_nomp'),(1,'n2vhJ4McHFfkNVGAcoz6p7fWaCehvtTSTH','p2pool');
+/*!40000 ALTER TABLE `cmd_pools_pubkey` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-09-08  3:48:18
