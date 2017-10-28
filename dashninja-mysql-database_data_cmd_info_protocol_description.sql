2f11857c96679a3ee2c666f7c9e8b166eba8bae3
Alexandre Devilliers <elbereth@dashninja.pl>
2017-10-28 21:42:44 +0200

    Latest cmd_info_protocol_description from 2017-10-28

--- dashninja-mysql-database_data_cmd_info_protocol_description.sql
+++ dashninja-mysql-database_data_cmd_info_protocol_description.sql
@@ -22,7 +22,6 @@
 /*!40101 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
@@ -33,17 +32,16 @@
 
 LOCK TABLES `cmd_info_protocol_description` WRITE;
 /*!40000 ALTER TABLE `cmd_info_protocol_description` DISABLE KEYS */;
-INSERT INTO `cmd_info_protocol_description` VALUES (0,'Unknown'),(70018,'RC3 (0.10.11.x)'),(70034,'RC4 (0.10.12.25)'),(70035,'RC4 (0.10.12.26+)'),(70038,'RC5 (0.10.13.14+)'),(70039,'Enforce (0.10.15.0+)'),(70043,'Onyx (0.10.15.13+)'),(70045,'Onyx v2 (0.10.16.5)'),(70046,'Onyx v2 (0.10.16.6+)'),(70048,'Onyx v3 (0.10.17.8+)'),(70050,'Onyx v3 (0.10.17.12+)'),(70051,'Onyx v3 (0.10.17.19+)'),(70052,'Core (0.11.0.0+)'),(70053,'Core (0.11.0.10+)'),(70054,'Core (0.11.0.11+)'),(70065,'InstantX (0.11.1.16+)'),(70066,'InstantX (0.11.1.19+)'),(70073,'Dash (0.11.2.7+)'),(70074,'Dash (0.11.2.8+)'),(70075,'Dash (0.11.2.11+)'),(70076,'Dash (0.11.2.18+)'),(70077,'Dash (0.12.0.0)'),(70078,'Dash (0.12.0.1)'),(70079,'Dash (0.12.0.2+)'),(70080,'Dash (0.12.0.4)'),(70081,'Dash (0.12.0.4)'),(70082,'Dash (0.12.0.4+)'),(70083,'Dash (0.12.0.7+)'),(70086,'Dash (0.12.0.12+)'),(70091,'Dash (0.12.0.20+)'),(70092,'Dash (0.12.0.22+)'),(70093,'Dash (0.12.0.25)'),(70094,'Dash (0.12.0.26+)'),(70098,'Dash (0.12.0.29)'),(70102,'Dash (0.12.0.44-9ee73cd)'),(70103,'Dash (0.12.0.44+)'),(70200,'Dash (0.12.1.0-0)'),(70201,'Dash (0.12.1.0-1)'),(70202,'Dash (0.12.1.0-2)'),(70203,'Dash (0.12.1.0-3)'),(70206,'Dash (0.12.1.0)');
+INSERT INTO `cmd_info_protocol_description` VALUES (0,'Unknown'),(70018,'RC3 (0.10.11.x)'),(70034,'RC4 (0.10.12.25)'),(70035,'RC4 (0.10.12.26+)'),(70038,'RC5 (0.10.13.14+)'),(70039,'Enforce (0.10.15.0+)'),(70043,'Onyx (0.10.15.13+)'),(70045,'Onyx v2 (0.10.16.5)'),(70046,'Onyx v2 (0.10.16.6+)'),(70048,'Onyx v3 (0.10.17.8+)'),(70050,'Onyx v3 (0.10.17.12+)'),(70051,'Onyx v3 (0.10.17.19+)'),(70052,'Core (0.11.0.0+)'),(70053,'Core (0.11.0.10+)'),(70054,'Core (0.11.0.11+)'),(70065,'InstantX (0.11.1.16+)'),(70066,'InstantX (0.11.1.19+)'),(70073,'Dash (0.11.2.7+)'),(70074,'Dash (0.11.2.8+)'),(70075,'Dash (0.11.2.11+)'),(70076,'Dash (0.11.2.18+)'),(70077,'Dash (0.12.0.0)'),(70078,'Dash (0.12.0.1)'),(70079,'Dash (0.12.0.2+)'),(70080,'Dash (0.12.0.4)'),(70081,'Dash (0.12.0.4)'),(70082,'Dash (0.12.0.4+)'),(70083,'Dash (0.12.0.7+)'),(70086,'Dash (0.12.0.12+)'),(70091,'Dash (0.12.0.20+)'),(70092,'Dash (0.12.0.22+)'),(70093,'Dash (0.12.0.25)'),(70094,'Dash (0.12.0.26+)'),(70098,'Dash (0.12.0.29)'),(70102,'Dash (0.12.0.44-9ee73cd)'),(70103,'Dash (0.12.0.44+)'),(70200,'Dash (0.12.1.0-0)'),(70201,'Dash (0.12.1.0-1)'),(70202,'Dash (0.12.1.0-2)'),(70203,'Dash (0.12.1.0-3)'),(70206,'Dash (0.12.1.0+)'),(70207,'Dash (0.12.2.0)'),(70208,'Dash (0.12.2.0)');
 /*!40000 ALTER TABLE `cmd_info_protocol_description` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-02-09 13:52:19
+-- Dump completed on 2017-10-28 21:35:38
